e6eea0b1535e71ed27d405217563d0deac3682d4
PatrickVonMassow-GIP <111737360+PatrickVonMassow-GIP@users.noreply.github.com>
2022-09-07 09:11:58 +0200

    18 replacement of ssh library preparation of exception handling (#22)
    * Git-18/DHICB-5112/XBE-608
    
    * Git-18/DHICB-5112/XBE-608
    
    Co-authored-by: johannesheucher-gip <107180330+johannesheucher-gip@users.noreply.github.com>

---
+++ modules/xact/connection/ssh/mdmimpl/SSHConnectionImpl/src/xact/ssh/Utils.java
@@ -0,0 +1,32 @@
+package xact.ssh;
+
+
+
+import xact.connection.SSHException;
+
+
+
+public final class Utils {
+
+  private Utils() {} // static utils class
+
+  static SSHException toSshException(net.schmizz.sshj.common.SSHException sshjException) {
+    switch (sshjException.getDisconnectReason()) {
+      case CONNECTION_LOST:
+        return new ConnectionLostException(sshjException.getMessage());
+      case HOST_KEY_NOT_VERIFIABLE:
+        return new HostKeyNotVerifiableException(sshjException.getMessage());
+      case HOST_NOT_ALLOWED_TO_CONNECT:
+        return new HostNotAllowedToConnectException(sshjException.getMessage());
+      case ILLEGAL_USER_NAME:
+        return new IllegalUserNameException(sshjException.getMessage());
+      case KEY_EXCHANGE_FAILED:
+        return new KeyExchangeFailedException(sshjException.getMessage());
+      case AUTH_CANCELLED_BY_USER:
+        return new UserAuthException(sshjException.getMessage());
+      default:
+        return new SSHException(sshjException.getMessage());
+    }
+  }
+
+}
