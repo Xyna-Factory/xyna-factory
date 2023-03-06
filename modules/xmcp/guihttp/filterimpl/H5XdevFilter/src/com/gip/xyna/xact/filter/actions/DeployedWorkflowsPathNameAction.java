9853d3987d41c3978f2590a494c2df9d81ca111d
Moritz-Schrauth-GIP <119330164+Moritz-Schrauth-GIP@users.noreply.github.com>
2023-03-06 08:01:31 +0100

    update outdated copyright headers (#172)

--- modules/xmcp/guihttp/filterimpl/H5XdevFilter/src/com/gip/xyna/xact/filter/actions/DeployedWorkflowsPathNameAction.java
+++ modules/xmcp/guihttp/filterimpl/H5XdevFilter/src/com/gip/xyna/xact/filter/actions/DeployedWorkflowsPathNameAction.java
@@ -1,15 +1,19 @@
-/*----------------------------------------------------
- * Xyna 6.1 (Black Edition)
- * Xyna GUI Support
- *----------------------------------------------------
- * Copyright GIP AG 2015
- * (http://www.gip.com)
- * Hechtsheimer Str. 35-37
- * 55131 Mainz
- *----------------------------------------------------
- * $Revision: 313343 $
- * $Date: 2022-08-19 11:53:44 +0200 (Fri, 19 Aug 2022) $
- *----------------------------------------------------
+/*
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2022 GIP SmartMercial GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 package com.gip.xyna.xact.filter.actions;
 
