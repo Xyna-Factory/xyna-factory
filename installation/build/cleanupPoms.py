a6a9a7cb6c10749db97cc01299be12bd39d1a052
Moritz Schrauth <moritz.schrauth@gip.com>
2023-03-17 13:26:39 +0100

    refactoring

--- installation/build/cleanupPoms.py
+++ installation/build/cleanupPoms.py
@@ -11,38 +11,38 @@ import glob
 CENTRAL_POM_PATH = os.path.normpath("../../installation/build/pom.xml")
 
 
-def removeInheritedDependencies(pom: io.TextIOWrapper) -> str:
+def remove_inherited_dependencies(pom: io.TextIOWrapper) -> str:
     """
     Goes through the contents and removes each <dependency> tag that doesn't specify a version.
 
     Updates the content of the file.
 
     We find all <dependency> tags with a regexp, then we go through each match 
-    and write from the last valid index to the start of the match. The last valid index is then set to the end of the match
+    and write from the last valid index to the start of the match. 
+    The last valid index is then set to the end of the match
     """
     content = pom.read()
     pom.seek(0)
-    lastIndex = 0
+    last_index = 0
     for match in re.finditer(r'<dependency>.*?</dependency>', content, re.DOTALL):
         # skip matches that have a version specified
         if "<version>" in content[match.start():match.end()]:
             continue
-        pom.write(content[lastIndex:match.start()])
-        lastIndex = match.end()
-    pom.write(content[lastIndex:])
+        pom.write(content[last_index:match.start()])
+        last_index = match.end()
+    pom.write(content[last_index:])
     pom.truncate()
 
 
 def main():
-    pomPaths = [os.path.normpath(path) for path in glob.glob("../../**/pom.xml", recursive=True)]
-    for pomXml in pomPaths:
+    pom_paths = [os.path.normpath(path) for path in glob.glob("../../**/pom.xml", recursive=True)]
+    for pom_xml in pom_paths:
         # ignore central pom.xml
-        if os.path.samefile(pomXml, CENTRAL_POM_PATH):
-            print(f"Skip {pomXml}")
+        if os.path.samefile(pom_xml, CENTRAL_POM_PATH):
             continue
 
-        with open(pomXml, "r+") as pomFile:
-            removeInheritedDependencies(pomFile)
+        with open(pom_xml, "r+") as pom_file:
+            remove_inherited_dependencies(pom_file)
 
 
 if __name__ == "__main__":
