a5b6ffec17c0ca07752b3efb036eec155da52104
Lukas Fey <lukas.fey@gip.com>
2023-04-19 09:17:33 +0200

    set output location

--- installation/build.sh
+++ installation/build.sh
@@ -304,7 +304,7 @@ build_xyna_factory() {
 
 #TODO: version name
 zip_result() {
-  zip -r ../XynaFactory_v0.0.0.0_000000_0000.zip $SCRIPT_DIR/../release
+  zip -r $SCRIPT_DIR/../XynaFactory_v0.0.0.0_000000_0000.zip $SCRIPT_DIR/../release
 }
 
 
