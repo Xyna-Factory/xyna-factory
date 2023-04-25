aea9aa5116cb1e5e8f76c20cac116bb4308dd86f
Cornelius Lippert | GIP <cornelius.lippert@gip.com>
2023-04-25 10:04:38 +0200

    current year in copyright header

--- server/src/com/gip/xyna/xfmg/xfctrl/appmgmt/ApplicationXmlEntry.java
+++ server/src/com/gip/xyna/xfmg/xfctrl/appmgmt/ApplicationXmlEntry.java
@@ -290,7 +290,7 @@ public class ApplicationXmlEntry {
   }
   
   private static final XynaPropertyString applicationXMLHeaderComment = new XynaPropertyString("xfmg.xfctrl.appmgmt.applicationxml.headercomment", "\n * - - - - - - - - - - - - - - - - - - - - - - - - - -\n" + 
-      " * Copyright 2022 GIP SmartMercial GmbH, Germany\n" + 
+      " * Copyright 2023 GIP SmartMercial GmbH, Germany\n" + 
       " *\n" +
       " * Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
       " * you may not use this file except in compliance with the License.\n" +
