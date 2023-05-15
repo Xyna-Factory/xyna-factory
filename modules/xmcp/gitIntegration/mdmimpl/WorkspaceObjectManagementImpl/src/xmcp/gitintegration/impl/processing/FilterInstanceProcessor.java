2ee141e27c384f1ee1cebac224a40b816c8f5389
Lukas Fey <107985579+LukasFey-GIP@users.noreply.github.com>
2023-05-15 09:27:20 +0200

    258 expand gitintegration (#263)
    * add new XMOM objects
    * add new datatypes to application.xml

---
+++ modules/xmcp/gitIntegration/mdmimpl/WorkspaceObjectManagementImpl/src/xmcp/gitintegration/impl/processing/FilterInstanceProcessor.java
@@ -0,0 +1,216 @@
+/*
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2023 GIP SmartMercial GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+package xmcp.gitintegration.impl.processing;
+
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.gip.xyna.XynaFactory;
+import com.gip.xyna.xact.XynaActivationPortal;
+import com.gip.xyna.xact.trigger.FilterInformation;
+import com.gip.xyna.xact.trigger.FilterInformation.FilterInstanceInformation;
+import com.gip.xyna.xnwh.persistence.PersistenceLayerException;
+import com.gip.xyna.xprc.xfractwfe.generation.xml.XmlBuilder;
+
+import xmcp.gitintegration.CREATE;
+import xmcp.gitintegration.DELETE;
+import xmcp.gitintegration.FilterInstance;
+import xmcp.gitintegration.MODIFY;
+import xmcp.gitintegration.WorkspaceContentDifference;
+
+
+
+public class FilterInstanceProcessor implements WorkspaceContentProcessor<FilterInstance> {
+
+  private static final String TAG_FILTERINSTANCE = "filterinstance";
+  private static final String TAG_FILTERINSTANCENAME = "filterinstancename";
+  private static final String TAG_FILTERNAME = "filtername";
+  private static final String TAG_TRIGGERINSTANCENAME = "triggerinstancename";
+
+  private static final XynaActivationPortal xynaActivationPortal = XynaFactory.getInstance().getActivationPortal();
+
+
+  @Override
+  public List<WorkspaceContentDifference> compare(Collection<? extends FilterInstance> from, Collection<? extends FilterInstance> to) {
+    List<WorkspaceContentDifference> wcdList = new ArrayList<WorkspaceContentDifference>();
+    List<FilterInstance> toWorkingList = new ArrayList<FilterInstance>();
+    if (to != null) {
+      toWorkingList.addAll(to);
+    }
+    HashMap<String, FilterInstance> toMap = new HashMap<String, FilterInstance>();
+    for (FilterInstance toEntry : toWorkingList) {
+      toMap.put(toEntry.getFilterInstanceName(), toEntry);
+    }
+
+    // iterate over from-list
+    // create MODIFY and DELETE entries
+    if (from != null) {
+      for (FilterInstance fromEntry : from) {
+        FilterInstance toEntry = toMap.get(fromEntry.getFilterInstanceName());
+        WorkspaceContentDifference wcd = new WorkspaceContentDifference();
+        wcd.setContentType(TAG_FILTERINSTANCE);
+        wcd.setExistingItem(fromEntry);
+        if (toEntry != null) {
+          if (!fromEntry.getFilterName().equals(toEntry.getFilterName())
+              || !fromEntry.getTriggerInstanceName().equals(toEntry.getTriggerInstanceName())) {
+            wcd.setDifferenceType(new MODIFY());
+            wcd.setNewItem(toEntry);
+            toWorkingList.remove(toEntry); // remove entry from to-list
+          } else {
+            toWorkingList.remove(toEntry); // remove entry from to-list
+            continue; // EQUAL -> ignore entry
+          }
+        } else {
+          wcd.setDifferenceType(new DELETE());
+        }
+        wcdList.add(wcd);
+      }
+    }
+
+    // iterate over toWorking-list (only CREATE-Entries remain)
+    for (FilterInstance toEntry : toWorkingList) {
+      WorkspaceContentDifference wcd = new WorkspaceContentDifference();
+      wcd.setContentType(TAG_FILTERINSTANCE);
+      wcd.setNewItem(toEntry);
+      wcd.setDifferenceType(new CREATE());
+      wcdList.add(wcd);
+    }
+    return wcdList;
+  }
+
+
+  @Override
+  public FilterInstance parseItem(Node node) {
+    FilterInstance fi = new FilterInstance();
+    NodeList childNodes = node.getChildNodes();
+    for (int i = 0; i < childNodes.getLength(); i++) {
+      Node childNode = childNodes.item(i);
+      if (childNode.getNodeName().equals(TAG_FILTERINSTANCENAME)) {
+        fi.setFilterInstanceName(childNode.getTextContent());
+      } else if (childNode.getNodeName().equals(TAG_FILTERNAME)) {
+        fi.setFilterName(childNode.getTextContent());
+      } else if (childNode.getNodeName().equals(TAG_TRIGGERINSTANCENAME)) {
+        fi.setTriggerInstanceName(childNode.getTextContent());
+      }
+    }
+    return fi;
+  }
+
+
+  @Override
+  public void writeItem(XmlBuilder builder, FilterInstance item) {
+    builder.startElement(TAG_FILTERINSTANCE);
+    builder.element(TAG_FILTERINSTANCENAME, item.getFilterInstanceName());
+    builder.element(TAG_FILTERNAME, item.getFilterName());
+    builder.element(TAG_TRIGGERINSTANCENAME, item.getTriggerInstanceName());
+    builder.endElement(TAG_FILTERINSTANCE);
+  }
+
+
+  @Override
+  public String getTagName() {
+    return TAG_FILTERINSTANCE;
+  }
+
+
+  @Override
+  public String createItemKeyString(FilterInstance item) {
+    return item.getFilterInstanceName();
+  }
+
+
+  @Override
+  public String createDifferencesString(FilterInstance from, FilterInstance to) {
+    StringBuffer ds = new StringBuffer();
+    if (!from.getFilterName().equals(to.getFilterName())) {
+      ds.append("\n");
+      ds.append("    " + TAG_FILTERNAME + " ");
+      ds.append(MODIFY.class.getSimpleName() + " \"" + from.getFilterName() + "\"=>\"" + to.getFilterName() + "\"");
+    }
+    if (!from.getTriggerInstanceName().equals(to.getTriggerInstanceName())) {
+      ds.append("\n");
+      ds.append("    " + TAG_TRIGGERINSTANCENAME + " ");
+      ds.append(MODIFY.class.getSimpleName() + " \"" + from.getTriggerInstanceName() + "\"=>\"" + to.getTriggerInstanceName() + "\"");
+    }
+    return ds.toString();
+  }
+
+
+  @Override
+  public List<FilterInstance> createItems(Long revision) {
+    List<FilterInstance> fiList = new ArrayList<FilterInstance>();
+    try {
+      List<FilterInstanceInformation> fiiList = getFilterInstanceInformationList(revision);
+      for (FilterInstanceInformation fii : fiiList) {
+        FilterInstance fi = new FilterInstance();
+        fi.setFilterInstanceName(fii.getFilterInstanceName());
+        fi.setFilterName(fii.getFilterName());
+        fi.setTriggerInstanceName(fii.getTriggerInstanceName());
+        fiList.add(fi);
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return fiList;
+  }
+
+
+  private List<FilterInstanceInformation> getFilterInstanceInformationList(Long revision) throws PersistenceLayerException {
+    List<FilterInstanceInformation> resultList = new ArrayList<FilterInstanceInformation>();
+    List<FilterInformation> filterInfoList = xynaActivationPortal.listFilterInformation();
+    for (FilterInformation filterInfo : filterInfoList) {
+      List<FilterInstanceInformation> filterInstInfoList = filterInfo.getFilterInstances();
+      for (FilterInstanceInformation filterInstInfo : filterInstInfoList) {
+        if (filterInstInfo.getRevision().longValue() == revision.longValue()) {
+          resultList.add(filterInstInfo);
+        }
+      }
+    }
+    return resultList;
+  }
+
+
+  @Override
+  public void create(FilterInstance item, long revision) {
+    // TODO Auto-generated method stub
+
+  }
+
+
+  @Override
+  public void modify(FilterInstance from, FilterInstance to, long revision) {
+    // TODO Auto-generated method stub
+
+  }
+
+
+  @Override
+  public void delete(FilterInstance item, long revision) {
+    // TODO Auto-generated method stub
+
+  }
+
+
+}
---
+++ modules/xmcp/gitIntegration/mdmimpl/WorkspaceObjectManagementImpl/src/xmcp/gitintegration/impl/processing/ReferenceSupport.java
@@ -0,0 +1,299 @@
+/*
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2023 GIP SmartMercial GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+package xmcp.gitintegration.impl.processing;
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.gip.xyna.XynaFactory;
+import com.gip.xyna.xfmg.xfctrl.revisionmgmt.RevisionManagement;
+import com.gip.xyna.xnwh.exceptions.XNWH_OBJECT_NOT_FOUND_FOR_PRIMARY_KEY;
+import com.gip.xyna.xprc.xfractwfe.generation.xml.XmlBuilder;
+
+import xmcp.gitintegration.CREATE;
+import xmcp.gitintegration.DELETE;
+import xmcp.gitintegration.MODIFY;
+import xmcp.gitintegration.Reference;
+import xmcp.gitintegration.WorkspaceContentDifferenceType;
+import xmcp.gitintegration.impl.ItemDifference;
+import xmcp.gitintegration.impl.OutputCreator;
+import xmcp.gitintegration.impl.references.ReferenceObjectType;
+import xmcp.gitintegration.impl.references.ReferenceType;
+import xmcp.gitintegration.storage.ReferenceStorable;
+import xmcp.gitintegration.storage.ReferenceStorage;
+
+
+
+/**
+ * 
+ * should not be a Processor, because ReferenceStorables
+ * are not supposed to be shared between factories.
+ * Instead, relevant WorkspaceContentItems contains reference
+ * tags. Reference tags contain a subset of the data stored
+ * in ReferenceStorable objects.
+ * 
+ * ReferenceStorable objects are specific to a factory (contain
+ * workspace revision), while the date provided by reference
+ * tags is factory-independent.
+ */
+public class ReferenceSupport {
+
+  public static final String TAG_REFERENCES = "references";
+  public static final String TAG_REFERENCE = "reference";
+  public static final String TAG_PATH = "path";
+  public static final String TAG_TYPE = "type";
+
+
+  public String getTagName() {
+    return TAG_REFERENCES;
+  }
+
+
+  public void create(String path, String objectType, String refType, Long wsRev, String objectName) {
+    //for command line/GUI
+    ReferenceStorage storage = new ReferenceStorage();
+    ReferenceStorable storable = new ReferenceStorable();
+    storable.setObjectName(objectName);
+    storable.setWorkspace(wsRev);
+    storable.setPath(path);
+    storable.setReftype(refType);
+    storable.setObjecttype(objectType);
+
+    List<String> issues = validateReference(storable);
+    if (!issues.isEmpty()) {
+      String issueString = String.join("\n", issues);
+      throw new RuntimeException("There are " + issues.size() + " problems with the reference: \n " + issueString);
+    }
+
+    storage.persist(storable);
+  }
+
+
+  public void create(Reference tag, long revision, String objectName, String objectType) {
+    //when merging workspace.xml
+    create(tag.getPath(), objectType, tag.getType().toString(), revision, objectName);
+  }
+
+
+  public void appendReferences(List<? extends Reference> tags, XmlBuilder builder) {
+    if(tags == null || tags.isEmpty()) {
+      return;
+    }
+    
+    builder.startElement(TAG_REFERENCES);
+    for (Reference tag : tags) {
+      appendReference(tag, builder);
+    }
+    builder.endElement(TAG_REFERENCES);
+  }
+
+
+  public void appendReference(Reference tag, XmlBuilder builder) {
+    builder.startElement(TAG_REFERENCE);
+    builder.element(TAG_TYPE, tag.getType());
+    builder.element(TAG_PATH, tag.getPath());
+    builder.endElement(TAG_REFERENCE);
+  }
+
+
+  /**
+   * Input should point to a node with getNodeName() equals TAG_REFERENCES
+   */
+  public List<Reference> parseTags(Node n) {
+    List<Reference> result = new ArrayList<Reference>();
+    if (n.getNodeName().equals(TAG_REFERENCES)) {
+      NodeList list = n.getChildNodes();
+      for (int i = 0; i < list.getLength(); i++) {
+        Node child = list.item(i);
+        if (child.getNodeName().equals(TAG_REFERENCE)) {
+          Reference tag = parse(child);
+          result.add(tag);
+        }
+      }
+    }
+    return result;
+  }
+
+
+  /**
+   * Input should point to a node with getNodeName() equals TAG_REFERENCE
+   */
+  public Reference parse(Node n) {
+    Reference result = new Reference();
+    NodeList list = n.getChildNodes();
+    for (int i = 0; i < list.getLength(); i++) {
+      Node child = list.item(i);
+      String nodeName = child.getNodeName();
+      if (nodeName.equals(TAG_PATH)) {
+        result.setPath(child.getTextContent());
+      } else if (nodeName.equals(TAG_TYPE)) {
+        result.setType(child.getTextContent());
+      }
+    }
+
+    return result;
+  }
+
+
+  public Reference convertToTag(ReferenceStorable storable) {
+    Reference result = new Reference();
+    result.setPath(storable.getPath());
+    result.setType(storable.getReftype());
+    return result;
+  }
+
+
+  private List<String> validateReference(ReferenceStorable storable) {
+    List<String> result = new ArrayList<String>();
+    RevisionManagement refMgmt = XynaFactory.getInstance().getFactoryManagement().getXynaFactoryControl().getRevisionManagement();
+    try {
+      ReferenceType.valueOf(storable.getReftype());
+    } catch (Exception e) {
+      result.add("Invalid ReferenceType. Available ReferenceTypes: " + Arrays.asList(ReferenceType.values()));
+    }
+
+    try {
+      ReferenceObjectType.valueOf(storable.getObjecttype());
+    } catch (Exception e) {
+      result.add("Invalid ObjectType. Available ObjectTypes: " + Arrays.asList(ReferenceObjectType.values()));
+    }
+
+    try {
+      refMgmt.getWorkspace(storable.getWorkspace());
+    } catch (XNWH_OBJECT_NOT_FOUND_FOR_PRIMARY_KEY e) {
+      result.add("Workspace not found.");
+    }
+
+    // storable.getPath(); does not get validated
+    // storable.getObjectName(); does not get validated
+
+    return result;
+  }
+
+
+  private static String keyMap(Reference t) {
+    return t.getPath();
+  }
+
+
+  //to may be null
+  private boolean compareReferenceTags(Reference from, Reference to) {
+    return to != null && Objects.equals(from.getPath(), to.getPath()) && Objects.equals(from.getType(), to.getType());
+  }
+
+
+  public List<ItemDifference<Reference>> compare(Collection<? extends Reference> from, Collection<? extends Reference> to) {
+    List<ItemDifference<Reference>> result = new ArrayList<ItemDifference<Reference>>();
+    from = from == null ? new ArrayList<Reference>() : from;
+    List<Reference> toList = to == null ? new ArrayList<Reference>() : new ArrayList<Reference>(to);
+    Map<String, Reference> toMap = toList.stream().collect(Collectors.toMap(ReferenceSupport::keyMap, Function.identity()));
+    String key;
+
+    // iterate over from-list
+    // create MODIFY and DELETE entries
+    for (Reference fromEntry : from) {
+      key = keyMap(fromEntry);
+      Reference toEntry = toMap.get(key);
+      toList.remove(toEntry);
+      if (!compareReferenceTags(fromEntry, toEntry)) {
+        Class<? extends WorkspaceContentDifferenceType> type = toEntry == null ? DELETE.class : MODIFY.class;
+        result.add(new ItemDifference<Reference>(type, fromEntry, toEntry));
+      }
+    }
+
+    // iterate over toWorking-list (only CREATE-Entries remain)
+    for (Reference tag : toList) {
+      result.add(new ItemDifference<Reference>(CREATE.class, null, tag));
+    }
+
+    return result;
+  }
+
+
+  private void appendDifference(StringBuilder ds, ItemDifference<Reference> difference) {
+    appendFormatted(ds, difference.getFrom());
+    if (difference.getType() == MODIFY.class) {
+      ds.append(" => ");
+    }
+    appendFormatted(ds, difference.getTo());
+    ds.append("\n");
+  }
+
+
+  /**
+   * reference may be null
+   */
+  private void appendFormatted(StringBuilder sb, Reference reference) {
+    if (reference == null) {
+      return;
+    }
+    sb.append(reference.getType()).append(": \"").append(reference.getPath()).append("\"");
+  }
+
+
+  public void createDifferencesString(StringBuilder ds, List<ItemDifference<Reference>> differences) {
+    OutputCreator creator = new OutputCreator();
+    creator.appendDiffs(ds, differences, TAG_REFERENCES, this::appendDifference);
+
+  }
+
+
+  public void modify(Reference from, Reference to, Long revision, String objectName, ReferenceObjectType objectType) {
+    ReferenceStorage storage = new ReferenceStorage();
+    storage.deleteReference(from.getPath(), revision, objectName);
+    ReferenceStorable storable = new ReferenceStorable();
+    storable.setObjectName(objectName);
+    storable.setWorkspace(revision);
+    storable.setPath(to.getPath());
+    storable.setReftype(to.getType().toString());
+    storable.setObjecttype(objectType.toString());
+    storage.persist(storable);
+  }
+
+
+  public void delete(String path, Long revision, String objectName) {
+    ReferenceStorage storage = new ReferenceStorage();
+    storage.deleteReference(path, revision, objectName);
+  }
+
+
+  public List<ReferenceStorable> getReferencetorableList(Long revision, String objectName) {
+    List<ReferenceStorable> resultList = new ArrayList<ReferenceStorable>();
+    ReferenceStorage storage = new ReferenceStorage();
+    List<ReferenceStorable> refStorablList = storage.getAllReferencesForType(revision, ReferenceObjectType.TRIGGER);
+    if (refStorablList != null) {
+      for (ReferenceStorable refStorable : refStorablList) {
+        if (refStorable.getObjectName().equals(objectName)) {
+          resultList.add(refStorable);
+        }
+      }
+    }
+    return resultList;
+  }
+}
