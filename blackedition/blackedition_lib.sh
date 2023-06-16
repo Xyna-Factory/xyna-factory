169109af2c64cdaae5d6112d8b250c96556c86b2
julianbrummer-gip <117432517+julianbrummer-gip@users.noreply.github.com>
2023-06-16 16:24:22 +0200

    support primitive type and documentation on insert (#330)

--- blackedition/blackedition_lib.sh
+++ blackedition/blackedition_lib.sh
@@ -30,7 +30,7 @@ ALL_DATAMODELTYPES=("mib","tr069","xsd");
 #ACHTUNG: Version auch bei addRequirement zu default workspace berücksichtigen
 ALL_APPLICATIONS="Base Processing"; #Default-Applications, die immer installiert sein sollten
 APPMGMTVERSION=1.0.10
-GUIHTTPVERSION=1.1.365
+GUIHTTPVERSION=1.1.366
 SNMPSTATVERSION=1.0.3
 PROCESSINGVERSION=1.0.17
 ALL_REPOSITORYACCESSES=("svn");
