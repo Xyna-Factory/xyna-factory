7a105ef3aebd85f2d1d5c84d0fa9e6073a0db455
Moritz Schrauth <moritz.schrauth@gip.com>
2023-06-23 14:48:48 +0200

    update copyright header

--- prerequisites/installation/install/prerequisites_lib.sh
+++ prerequisites/installation/install/prerequisites_lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Copyright 2022 Xyna GmbH, Germany
+# Copyright 2023 Xyna GmbH, Germany
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
