a4ffca49b72b77c99c0b1b958542573a98d8ad66
Moritz Schrauth <moritz.schrauth@gip.com>
2023-07-03 10:03:24 +0200

    add checkEncoding script

---
+++ installation/build/checkEncoding.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+# -*- coding: iso-8859-15 -*-
+"""
+Goes through all the files in the given directory and checks, that their encoding is ISO-8859-1.
+"""
+
+import sys
+import argparse
+from pathlib import Path
+
+
+REPO_ROOT = Path(sys.argv[0]).absolute().parents[2]
+VERBOSE = False
+
+VIOLATING_FILES: list[Path] = []
+
+ENCODING_WITNESSES: list[str] = [
+    "ö",
+    "ä",
+    "ü",
+    "Ö",
+    "�",
+]
+
+
+def ignore_dir(directory: Path) -> bool:
+    """
+    Checks if the directory shall be ignored (i.e., `.git/`)
+
+    Returns:
+        `True` if this directory shall be ignored; `False` if not
+    """
+    return ".git" in str(directory)
+
+
+def ignore_file(file: Path) -> bool:
+    """
+    Checks if the file shall be ignored (i.e., `*.jar`)
+
+    Returns:
+        `True` if the file shall be ignored; `False` if not
+    """
+    return ".jar" in file.name \
+        or ".zip" in file.name \
+        or "OtherExportImportAndUtils.java" in file.name
+
+
+def encoding_ok(file: Path) -> bool:
+    """
+    Checks if `file` is ISO-8859-1 encoded or not
+
+    Args:
+        `file`  Path to file to be checked
+
+    Returns:
+        `True` if file is ISO-8859-1 encoded; `False` if not
+    """
+    if ignore_file(file):
+        return True
+
+    try:
+        content = file.read_text(encoding="ISO-8859-1", errors="strict")
+        found_witness = any(
+            [witness in content for witness in ENCODING_WITNESSES])
+        return not found_witness
+    except ValueError as e:
+        print(f"Got reading error: {e}")
+        return False
+
+
+def find_files(directory: Path, report_all: bool):
+    """
+    Recursively searches through the given directory and adds files in utf-8 encoding to `VIOLATING_FILES`
+    """
+    global VIOLATING_FILES, VERBOSE
+    if not directory.is_dir() or ignore_dir(directory):
+        return
+
+    if VERBOSE:
+        print(f"Search through directory {directory}")
+    for entry in directory.iterdir():
+        if entry.is_dir():
+            find_files(entry, report_all)
+        elif not encoding_ok(entry):
+            VIOLATING_FILES.append(entry)
+            if not report_all:
+                return
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", default=str(REPO_ROOT),
+                        help="Directory to recursively search through")
+    parser.add_argument("--report-all", action="store_true",
+                        help="First find all violating files and reporting them before exiting")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    args = parser.parse_args()
+
+    search_directory = Path(args.directory).resolve()
+    VERBOSE = args.verbose
+
+    find_files(search_directory, args.report_all)
+
+    print("")
+    if len(VIOLATING_FILES) > 0:
+        sys.stderr.write(
+            f"Found {len(VIOLATING_FILES)} files(s) not in ISO-8859-1 encoding:\n")
+        sys.stderr.writelines([f" - {file}\n" for file in VIOLATING_FILES])
+        sys.exit(1)
+
+    print("All files ok")
+
+
+if __name__ == "__main__":
+    main()
