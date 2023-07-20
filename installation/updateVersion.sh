696f9477f4d3d37ac0b1130e718cc535947ba008
Lukas Fey <lukas.fey@gip.com>
2023-07-20 08:50:36 +0200

    * use ./updateVersion.sh <newVersion> to update version for xynafactory.jar in server/pom.xml and installation/build/pom.xml as well as installation/delivery/delivery.properties * build.sh no longer references a specific xynafactory.jar version

---
+++ installation/updateVersion.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# < 1 ]]; then
+  echo "provide a new version."
+  exit 1
+fi
+
+set -e
+
+VERSION=$1
+INSTALL_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SERVER_POM=$INSTALL_DIR/../server/pom.xml
+BOM=$INSTALL_DIR/build/pom.xml
+DELIVERY=$INSTALL_DIR/delivery/delivery.properties
+
+mvn -f "$SERVER_POM" versions:set -DnewVersion="$VERSION" -DartifactId=xynafactory -DgroupId=com.gip.xyna -DprocessDependencies=true
+mvn -f "$BOM" versions:use-dep-version -DdepVersion="$VERSION" -Dincludes=com.gip.xyna:xynafactory -DforceVersion=true
+sed -i "s#release\.number=v.*#release\.number=v$VERSION#" $DELIVERY
+
+echo "Version updated to $VERSION"
+exit 0
