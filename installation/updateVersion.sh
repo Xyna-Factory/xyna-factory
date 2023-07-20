537733c58b97fb9d35e532c51acc4892d880180c
Lukas Fey <lukas.fey@gip.com>
2023-07-20 09:42:05 +0200

    add version format check

--- installation/updateVersion.sh
+++ installation/updateVersion.sh
@@ -8,6 +8,13 @@ fi
 set -e
 
 VERSION=$1
+FORMAT="^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$"
+
+if [[ ! $VERSION =~ $FORMAT ]]; then
+        echo "Invalid version format. Expected format: \"$FORMAT\"."
+        exit 1
+fi
+
 INSTALL_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVER_POM=$INSTALL_DIR/../server/pom.xml
 BOM=$INSTALL_DIR/build/pom.xml
