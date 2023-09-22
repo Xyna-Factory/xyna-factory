e80fde2f2b065a398e64465f59f89493fbddfa34
Oliver Sünder <oliver.suender@gip.com>
2023-09-22 11:58:32 +0200

    v9.0.3.2

--- server/src/com/gip/xyna/update/Updater.java
+++ server/src/com/gip/xyna/update/Updater.java
@@ -1783,6 +1783,10 @@ public class Updater implements UpdaterInterface {
       ud = new UpdateJustVersion(v468, v469, false);
       updates.add(ud);
 
+      Version v470 = new Version(v469).increaseToMajorVersion(4, 1);
+      ud = new UpdateJustVersion(v469, v470, false);
+      updates.add(ud);
+
       //ACHTUNG: bei updates in einem branch muss gewährleistet werden, dass alle späteren versionen (trunk, spätere branches)
       //         auf dem branch updaten können. bei updates, die in späteren versionen dann sonderbehandlungen im update-
       //         prozess benötigen, müssen die versionen, auf denen die updates passieren sollen, oben genau angegeben werden.
