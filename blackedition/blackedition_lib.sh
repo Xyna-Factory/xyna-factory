67fda72a3a0d8e46fb547ff8278ed00afb401fe5
Lukas Fey <lukas.fey@gip.com>
2023-10-16 13:50:19 +0200

    GuiHttp 1.1.373 - set documentation when changing operation type

--- blackedition/blackedition_lib.sh
+++ blackedition/blackedition_lib.sh
@@ -30,7 +30,7 @@ ALL_DATAMODELTYPES=("mib","tr069","xsd");
 #ACHTUNG: Version auch bei addRequirement zu default workspace berücksichtigen
 ALL_APPLICATIONS="Base Processing"; #Default-Applications, die immer installiert sein sollten
 APPMGMTVERSION=1.0.10
-GUIHTTPVERSION=1.1.372
+GUIHTTPVERSION=1.1.373
 SNMPSTATVERSION=1.0.3
 PROCESSINGVERSION=1.0.17
 ALL_REPOSITORYACCESSES=("svn");
