4969057213bcc109b2cdd01b53b864bb001b4c15
Julian-Mueller-GIP <julian.mueller@gip.com>
2024-01-17 14:30:42 +0100

    fman refactoring new dependency Runtime Context Support version 1.0.0 for xyna release

--- modules/xfmg/oas/change_app_version.sh
+++ modules/xfmg/oas/change_app_version.sh
@@ -8,7 +8,6 @@ if [[ $1 =~ ^$versionregex$ ]]
 then
     sed -i -E "s/(versionName=\")$versionregex/\1$1/" application.xml
     sed -i -E "s/(<VersionName>)$versionregex(<\/VersionName>)/\1$1\2/" sharedlib/xyna-openapi/generators/xmom-data-model/src/main/resources/xmom-data-model/application.mustache
-    sed -i -E "s/(<Mapping>%0%.version=\")$versionregex(\"<\/Mapping>)/\1$1\2/" XMOM/xmcp/oas/fman/GetOASImportHistoryDefinition.xml
 else
     echo "invalid version format"
 fi
