b446d1c2e0b8e2b509eaf64f370ee36be1452aed
Oliver Sünder <115245174+osuender@users.noreply.github.com>
2024-02-23 10:38:05 +0100

    717 extend buildsh with the optional parameter to specify the used xyna modeller branch (#718)

--- installation/build.sh
+++ installation/build.sh
@@ -19,8 +19,10 @@
 set -e
 
 print_help() {
-  echo "$0: build some or all parts of xyna."
-  echo "available options are: xynautils, build, all, compose"
+  echo "Usage: $0 xynautils"
+  echo "Usage: $0 build"
+  echo "Usage: $0 all -b GIT_BRANCH_XYNA_MODELLER"
+  echo "Usage: $0 compose"
 }
 
 check_dependencies() {
@@ -283,11 +285,15 @@ build_prerequisites() {
   ant -f delivery.xml
 }
 
-build_modeller() {
-  MODELLER_TAG=$(cat ${SCRIPT_DIR}/delivery/delivery.properties | grep ^xynamodeller.release.tag | cut -d'=' -f2) #e.g. 9.0.0.0
-  echo "building Modeller GUI from tag ${MODELLER_TAG}"
+build_modeller() { 
+  if [[ -z ${GIT_BRANCH_XYNA_MODELLER} ]]; then
+    RELEASE_NUMBER=$(cat ${SCRIPT_DIR}/delivery/delivery.properties | grep ^release.number | cut -d'=' -f2)
+    # branch is RELEASE_NUMBER without the first 'v'
+    GIT_BRANCH_XYNA_MODELLER=${RELEASE_NUMBER:1}
+  fi
+  echo "building Modeller GUI from branch ${GIT_BRANCH_XYNA_MODELLER}"
   cd $SCRIPT_DIR/build
-  ant -f build-gui.xml -Dmodeller.tag=${MODELLER_TAG}
+  ant -f build-gui.xml -Dmodeller.branch=${GIT_BRANCH_XYNA_MODELLER}
 }
 
 build_xyna_factory() {
@@ -632,16 +638,11 @@ build() {
 }
 
 
-# main
-if [ $# -eq 0 ]
-then
-  print_help
-  exit 0
-fi
-
 check_dependencies
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 prepare_build
+
+IT_BRANCH_XYNA_MODELLER=""
 
 case $1 in
   "xynautils")
@@ -651,6 +652,18 @@ case $1 in
     build
     ;;
   "all")
+    OPTIND=2
+    while getopts ":b:" options; do
+      case "${options}" in 
+        b)
+          GIT_BRANCH_XYNA_MODELLER=${OPTARG}
+          ;;
+        *) # If unknown (any other) option:
+          print_help
+          exit 1
+          ;;
+      esac
+    done
     check_dependencies_frontend
     build_all
     ;;
@@ -658,7 +671,7 @@ case $1 in
     build_xyna_factory
     ;;
   *)
-    echo "unknown argument: $1"
+    print_help
     exit 1
     ;;
 esac
