d6972a25ff2cde45a0bf8909ad426290efe6be0d
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2024-05-27 12:59:41 +0200

    Order Types: layout disrupts overview

--- modules/xmcp/guihttp/mdmimpl/OrderTypeServicesImpl/src/xmcp/factorymanager/impl/converter/OrderTypeConverter.java
+++ modules/xmcp/guihttp/mdmimpl/OrderTypeServicesImpl/src/xmcp/factorymanager/impl/converter/OrderTypeConverter.java
@@ -51,7 +51,8 @@ public class OrderTypeConverter {
     OrderType r = new OrderType();
     r.setDocumentation(in.getDocumentation());
     if(in.getExecutionDestinationValue() != null)
-      r.setExecutionDestination(filter ? convert(in.getExecutionDestinationValue(), filter) : convert(in.getExecutionDestinationValue(), filter).substring(in.getExecutionDestinationValue().lastIndexOf('.')) + 1);
+      //r.setExecutionDestination(filter ? convert(in.getExecutionDestinationValue(), filter) : convert(in.getExecutionDestinationValue(), filter).substring(in.getExecutionDestinationValue().lastIndexOf('.')) + 1);
+      r.setExecutionDestination(convert(in.getExecutionDestinationValue(), filter));
     if(in.getMonitoringLevel() != null)
       r.setMonitoringLevel(String.valueOf(in.getMonitoringLevel()));
     r.setEvaluatedMonitoringLevel(in.getMonitoringLevel());
