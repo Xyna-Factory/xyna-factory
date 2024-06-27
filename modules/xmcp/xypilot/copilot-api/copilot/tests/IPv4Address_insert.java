93323c0172c172433e5a8fc82567df3f11bfe7b5
Simon Franz <simon.franz@gip.com>
2024-06-27 13:47:29 +0200

    added more Copyright header and fixed date on already added ones

--- modules/xmcp/xypilot/copilot-api/copilot/tests/IPv4Address_insert.java
+++ modules/xmcp/xypilot/copilot-api/copilot/tests/IPv4Address_insert.java
@@ -1,3 +1,22 @@
+/*
+ *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2024 Xyna GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+
+
 /**
  * IPv4 addresses are 32-bit integers, but they are usually represented as a string of four numbers separated by dots.
  */
