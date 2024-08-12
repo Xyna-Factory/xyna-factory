068e100374d19c9a2d7240736049e6dbbfd40cca
Lukas Fey <lukas.fey@gip.com>
2024-08-12 09:39:29 +0200

    create application - copy and fill templates

--- installation/build/createApplication.py
+++ installation/build/createApplication.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import argparse
 import os
 import shutil
@@ -6,14 +6,15 @@ from pathlib import Path
 
 class AppCreator:
 
+  def __init__(self):
+    self.this_dir = Path(__file__).parent.absolute()
+
   def copy_template(self, target_dir: str, file_name: str):
-    this_dir = Path(__file__).parent.absolute()
-    path_to_template = os.path.join(this_dir, "templates", f"{file_name}.template")
+    path_to_template = os.path.join(self.this_dir, "templates", f"{file_name}.template")
     shutil.copy(path_to_template, os.path.join(target_dir, file_name))
 
   def copy_and_set_application_xml(self, appDir: str, appName: str, appVersion: str):
-    this_dir = Path(__file__).parent.absolute()
-    application_xml_template = os.path.join(this_dir, "templates", "application.xml.template")
+    application_xml_template = os.path.join(self.this_dir, "templates", "application.xml.template")
     with open(application_xml_template, 'r') as file:
       filedata = file.read()
     filedata = filedata.replace('{{APPNAME}}', appName)
@@ -22,9 +23,21 @@ class AppCreator:
     with open(application_xml, 'w') as file:
       file.write(filedata)
 
+  def copy_and_set_project_bom_xml(self, projectDir: str, groupId: str):
+    relpath = os.path.relpath(self.this_dir.parent.parent.absolute(), Path(app_dir).absolute())
+    relpath = relpath.replace("\\", "/")
+    relpath += "/installation/build/pom.xml"
+    project_bom_xml_template = os.path.join(self.this_dir, "templates", "project.pom.xml.template")
+    with open(project_bom_xml_template, 'r') as file:
+      filedata = file.read()
+    filedata = filedata.replace('{{XYNABOM}}', relpath)
+    filedata = filedata.replace('{{groupId}}', groupId)
+    project_bom_xml = os.path.join(projectDir, "pom.xml")
+    with open(project_bom_xml, 'w') as file:
+      file.write(filedata)
+
   def copy_and_set(self, target_dir: str, file_name: str, replacements: list[[str, str]]):
-    this_dir = Path(__file__).parent.absolute()
-    path_to_template = os.path.join(this_dir, "templates", f"{file_name}.template")
+    path_to_template = os.path.join(self.this_dir, "templates", f"{file_name}.template")
     with open(path_to_template, 'r') as file:
       filedata = file.read()
     for (key, value) in replacements:
@@ -34,20 +47,23 @@ class AppCreator:
       file.write(filedata)
 
   def createApp(self, projectDir: str, appDir: str, appName: str, appVersion: str):
-    this_dir = Path(__file__).parent.absolute()
+    next_steps = []
     app_dir = os.path.join(projectDir, appDir)
     print(f"creating application {appName}/{appVersion} at app_dir")
     if not os.path.isdir(projectDir):
-      print("project directory does not exist. Creating it and placing server.properties in it.")
+      print("project directory does not exist. Creating it and placing server.properties and pom.xml in it.")
       os.makedirs(projectDir, exist_ok= True)
       self.copy_template(projectDir, "server.properties" )
+      self.copy_and_set_project_bom_xml(self, projectDir, "myproject")
+      next_steps.append("* set server.properties")
+      next_steps.append("* set groupId in pom.xml")
     else:
       print("project directory exists already.")
     if os.path.isdir(app_dir):
       print("application exists already! Terminating")
       return
     
-    relpath = os.path.relpath(this_dir.parent.parent.absolute(), Path(app_dir).absolute())
+    relpath = os.path.relpath(self.this_dir.parent.parent.absolute(), Path(app_dir).absolute())
     relpath = relpath.replace("\\", "/")
     print(f"relpath: {relpath}")
     
@@ -55,8 +71,10 @@ class AppCreator:
     self.copy_template(app_dir, "application.properties" )
     self.copy_and_set(app_dir, "application.xml", [("{{APPNAME}}", appName),("{{VERSIONNAME}}", appVersion)])
     self.copy_and_set(app_dir, "build.xml", [("{{ROOTDIRPATH}}", relpath)])
-    print("Done")
-    
+      next_steps.append("* set application.properties")
+    print("Done. Next steps:")
+    for step in next_steps:
+      print(step)
     
 
 
