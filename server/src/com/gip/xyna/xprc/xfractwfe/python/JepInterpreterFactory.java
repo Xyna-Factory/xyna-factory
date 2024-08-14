efc2ca66b868ab2e526154aa4ede45423576d974
Julian-Mueller-GIP <julian.mueller@gip.com>
2024-08-14 14:30:41 +0200

    implementation of method convertToPython

--- server/src/com/gip/xyna/xprc/xfractwfe/python/JepInterpreterFactory.java
+++ server/src/com/gip/xyna/xprc/xfractwfe/python/JepInterpreterFactory.java
@@ -19,31 +19,39 @@ package com.gip.xyna.xprc.xfractwfe.python;
 
 
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 
 import com.gip.xyna.CentralFactoryLogging;
 import com.gip.xyna.XynaFactory;
 import com.gip.xyna.xdev.xfractmod.xmdm.GeneralXynaObject;
+import com.gip.xyna.xdev.xfractmod.xmdm.XynaExceptionBase;
+import com.gip.xyna.xdev.xfractmod.xmdm.XynaObject;
 import com.gip.xyna.xfmg.xfctrl.classloading.ClassLoaderBase;
 import com.gip.xyna.xfmg.xfctrl.xmomdatabase.XMOMDatabase;
 import com.gip.xyna.xfmg.xfctrl.xmomdatabase.XMOMDatabaseType;
 import com.gip.xyna.xfmg.xfctrl.xmomdatabase.search.XMOMDatabaseSearchResult;
 import com.gip.xyna.xfmg.xfctrl.xmomdatabase.search.XMOMDatabaseSearchResultEntry;
 import com.gip.xyna.xfmg.xfctrl.xmomdatabase.search.XMOMDatabaseSelect;
+import com.gip.xyna.xprc.xfractwfe.InvalidObjectPathException;
+
 
 
 public class JepInterpreterFactory extends PythonInterpreterFactory {
 
   private static final Logger logger = CentralFactoryLogging.getLogger(JepInterpreterFactory.class);
   private Map<Long, Set<String>> packagesPerRevision = new HashMap<>();
-  
+
+
   @Override
   public PythonInterpreter createInterperter(ClassLoaderBase classLoader) {
     Long revision = classLoader.getRevision();
@@ -82,16 +90,71 @@ public class JepInterpreterFactory extends PythonInterpreterFactory {
       packagesPerRevision.remove(rev);
     }
   }
-  
+
+
   @Override
   public Map<String, Object> convertToPython(GeneralXynaObject obj) {
-    return null;
+    Set<String> varNames = new HashSet<String>();
+    Map<String, Object> resultMap = new HashMap<String, Object>();
+
+    GeneralXynaObject convertObj = null;
+
+    if (obj instanceof XynaObject) {
+      convertObj = (XynaObject) obj;
+      resultMap.put("_xynatype", "DATATYPE");
+      resultMap.put("_fqn", obj.getClass().getCanonicalName());
+      varNames = ((XynaObject) obj).getVariableNames();
+    } else if (obj instanceof XynaExceptionBase) {
+      convertObj = (XynaExceptionBase) obj;
+      resultMap.put("_xynatype", "EXCEPTION");
+      resultMap.put("_fqn", convertObj.getClass().getCanonicalName());
+      for (Field f : convertObj.getClass().getDeclaredFields()) {
+        if (f.getModifiers() == 2) { // private members
+          varNames.add(f.getName());
+        }
+      }
+    } else {
+      //throw new UnsupportedOperationException();
+      return new HashMap<>();
+    }
+
+    for (String i : varNames) {
+      try {
+        Object memberObj = convertObj.get(i);
+        if (memberObj instanceof XynaObject) {
+          resultMap.put(i, convertToPython((XynaObject) memberObj));
+        } else if (memberObj instanceof XynaExceptionBase) {
+          resultMap.put(i, convertToPython((XynaExceptionBase) memberObj));
+        } else if (memberObj instanceof List) {
+          List<?> memberObjList = (List<?>) memberObj;
+          if (memberObjList.size() > 0) {
+            if (memberObjList.get(0) instanceof GeneralXynaObject) {
+              List<Object> resultList = new ArrayList<Object>();
+              for (Object o : memberObjList) {
+                resultList.add(convertToPython((GeneralXynaObject) o));
+              }
+              resultMap.put(i, resultList);
+            } else {
+              resultMap.put(i, memberObjList);
+            }
+          }
+        } else {
+          resultMap.put(i, memberObj);
+        }
+      } catch (InvalidObjectPathException e) {
+        throw new RuntimeException("Could not load variable names from " + convertObj, e);
+      }
+    }
+
+    return resultMap;
   }
+
 
   @Override
   public GeneralXynaObject convertToJava(Context context, Object obj) {
     return null;
   }
+
 
   @Override
   public Object invokeService(Context context, String fqn, String serviceName, Object... args) {
