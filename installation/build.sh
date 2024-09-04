a9154d65232a5de949124e55f36b53e9561c9920
Oliver Sünder <115245174+osuender@users.noreply.github.com>
2024-09-04 13:48:21 +0200

    1105 remove unnecessary defaultmavensettingsxml (#1110)

--- installation/build.sh
+++ installation/build.sh
@@ -591,8 +591,6 @@ compose_modeller() {
 prepare_build() {
   mkdir -p /opt/common
   cd $SCRIPT_DIR/build
-  sed -i "s#<url>file:\/\/.*<\/url>#<url>file://$HOME/.m2/repository<\/url>#" $SCRIPT_DIR/build/defaultMavenSettings.xml #TODO: allow config
-  sed -i "s#<localRepository>.*</localRepository>#<localRepository>//$HOME/.m2/repository</localRepository>#" $SCRIPT_DIR/build/defaultMavenSettings.xml
   mvn install
 }
 
