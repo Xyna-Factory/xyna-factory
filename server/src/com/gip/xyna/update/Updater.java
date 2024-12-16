813fb7ccbc3c54186690b6225ec0815b05de3378
Oliver Sünder <115245174+osuender@users.noreply.github.com>
2024-12-16 12:02:23 +0100

    1300-update-patch-version-to-10201 (#1302)

--- server/src/com/gip/xyna/update/Updater.java
+++ server/src/com/gip/xyna/update/Updater.java
@@ -1810,6 +1810,11 @@ public class Updater implements UpdaterInterface {
       ud.addFollowingBranchVersionsAsAllowedForUpdate(3); //10.1.x.x darf hier drauf updaten
       updates.add(ud);
 
+      // 10.2.0.1
+      Version v474 = new Version(v473).increaseToMajorVersion(4, 1);
+      ud = new UpdateJustVersion(v473, v474);
+      updates.add(ud);
+
       //ACHTUNG: bei updates in einem branch muss gewährleistet werden, dass alle späteren versionen (trunk, spätere branches)
       //         auf dem branch updaten können. bei updates, die in späteren versionen dann sonderbehandlungen im update-
       //         prozess benötigen, müssen die versionen, auf denen die updates passieren sollen, oben genau angegeben werden.
