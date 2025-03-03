3bd8eb06a0d1b52e42b14faf860fa0768aad1a45
Ulrich Seul <ulrich.seul@gip.com>
2025-03-03 22:21:52 +0100

    backup devel state

---
+++ modules/xmcp/gitIntegration/mdmimpl/WorkspaceObjectManagementImpl/src/xmcp/gitintegration/impl/WorkspaceStatusTools.java
@@ -0,0 +1,142 @@
+/*
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2025 Xyna GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+
+package xmcp.gitintegration.impl;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.log4j.Logger;
+
+import base.Text;
+import xmcp.gitintegration.InfoWorkspaceContentDiffGroup;
+import xmcp.gitintegration.InfoWorkspaceContentDiffGroupList;
+import xmcp.gitintegration.InfoWorkspaceContentDiffItem;
+import xmcp.gitintegration.ListId;
+import xmcp.gitintegration.WorkspaceContent;
+import xmcp.gitintegration.WorkspaceContentDifference;
+import xmcp.gitintegration.WorkspaceContentDifferences;
+import xmcp.gitintegration.WorkspaceContentItem;
+import xmcp.gitintegration.impl.processing.WorkspaceContentProcessingPortal;
+import xmcp.gitintegration.storage.WorkspaceDifferenceListStorage;
+import xprc.xpce.Workspace;
+
+
+public class WorkspaceStatusTools {
+
+  private static Logger _logger = Logger.getLogger(WorkspaceStatusTools.class);
+  
+  
+  public Text getWorkspaceXmlStatus() {
+    Text ret = new Text();
+    ret.setText("status ok");
+    return ret;
+  }
+    
+  
+  public void updateWorkspaceContent(Workspace workspace) {
+    
+  }
+  
+  
+  public WorkspaceContent createWorkspaceContentFromFile(base.File fileIn) {
+    if (fileIn == null) { throw new IllegalArgumentException("Parameter file is empty"); }
+    if (fileIn.getPath() == null) { throw new IllegalArgumentException("Parameter file is empty"); }
+    WorkspaceContentCreator creator = new WorkspaceContentCreator();
+    File file = new File(fileIn.getPath());
+    WorkspaceContent ret = creator.createWorkspaceContentFromFile(file);
+    return ret;
+  }
+  
+  
+  public InfoWorkspaceContentDiffGroupList adaptWorkspaceDifferenceList(ListId listid) {
+    if (listid == null) { throw new IllegalArgumentException("Parameter list id is empty"); }
+    if (listid.getListId() == null) { throw new IllegalArgumentException("Parameterlist id is empty"); }
+    InfoWorkspaceContentDiffGroupList ret = new InfoWorkspaceContentDiffGroupList();
+    Map<String, InfoWorkspaceContentDiffGroup> groupmap = new TreeMap<>();
+    try {
+      WorkspaceContentProcessingPortal portal = new WorkspaceContentProcessingPortal();
+      WorkspaceDifferenceListStorage storage = new WorkspaceDifferenceListStorage();
+      //WorkspaceContentDifferences difflist = storage.loadDifferences(listid.getListId());
+      List<? extends WorkspaceContentDifferences> alldiffs = storage.loadAllDifferencesLists();
+      WorkspaceContentDifferences difflist = null;
+      for (WorkspaceContentDifferences wcd : alldiffs) {
+        if (listid.getListId() == wcd.getListId()) {
+          difflist = wcd;
+          break;
+        }
+      }
+      if (difflist == null) { return ret; }
+      if (difflist.getDifferences() == null) { return ret; }
+      for (WorkspaceContentDifference diff : difflist.getDifferences()) {
+        OutputCreator<WorkspaceContentItem, WorkspaceContentDifference, WorkspaceContentItemDifferenceSelector> outputCreator = 
+          new OutputCreator<WorkspaceContentItem, WorkspaceContentDifference, WorkspaceContentItemDifferenceSelector>(
+            new WorkspaceContentItemDifferenceSelector());
+        String output = outputCreator.createOutput(diff, portal);
+        InfoWorkspaceContentDiffGroup group = groupmap.get(diff.getContentType());
+        if (group == null) {
+          group = new InfoWorkspaceContentDiffGroup();
+          group.setContentType(diff.getContentType());
+          ret.addToInfoWorkspaceContentDiffGroup(group);
+          group.setGroupIndex(ret.getInfoWorkspaceContentDiffGroup().size() - 1);
+          groupmap.put(diff.getContentType(), group);
+        }
+        InfoWorkspaceContentDiffItem item = new InfoWorkspaceContentDiffItem();
+        item.setDifferenceInfo(output);
+        item.setDifferenceType(diff.getDifferenceType());
+        group.addToDifferenceList(item);
+        item.setItemIndex(group.getDifferenceList().size() - 1);
+        item.setNumberOfLines(output.split("\n").length);
+      }      
+    }
+    catch (Exception e) {
+      _logger.error(e.getMessage(), e);
+      throw new RuntimeException(e.getMessage(), e);
+    }
+    return ret;
+  }
+
+
+  public base.File getPathToWorkspaceXml(Workspace workspace) {
+    if (workspace == null) { throw new IllegalArgumentException("Parameter workspace is empty"); }
+    if (workspace.getName() == null) { throw new IllegalArgumentException("Parameter workspace is empty"); }
+    WorkspaceContentCreator creator = new WorkspaceContentCreator();
+    File file = creator.determineWorkspaceXMLFile(workspace.getName());
+    base.File ret = new base.File();
+    ret.setPath(file.getPath());
+    return ret;
+  }
+  
+  
+  /*
+  public WorkspaceContentDifferences compareWorkspaceXmlWithCurrentConfig(Workspace workspace) {
+    
+    String workspaceName = workspace.getName();
+    WorkspaceContentCreator creator = new WorkspaceContentCreator();
+    File file = creator.determineWorkspaceXMLFile(workspaceName);
+    WorkspaceContent xmlConfig = creator.createWorkspaceContentFromFile(file);
+    WorkspaceContent factoryConfig = creator.createWorkspaceContentForWorkspace(workspaceName);
+    WorkspaceContentComparator comparator = new WorkspaceContentComparator();
+    WorkspaceContentDifferences differences = comparator.compareWorkspaceContent(factoryConfig, xmlConfig, true);
+    return differences;
+  }
+  */
+  
+}
