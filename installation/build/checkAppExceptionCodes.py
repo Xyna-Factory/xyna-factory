5970f1316b1d95cbab46e3c054ab29c05f0acd44
Oliver Sünder <oliver.suender@gip.com>
2025-04-25 20:56:52 +0200

    1627-add-checkappexceptioncodespy

--- installation/build/checkAppExceptionCodes.py
+++ installation/build/checkAppExceptionCodes.py
@@ -60,7 +60,7 @@ class ExceptionXmlUtils:
     processed_exception_info_list = []
     for xmom_path, exception_info_list in all_exception_info_by_xmom_path.items():
       processed_exception_info_list = self.check_code_prefix(xmom_path, exception_info_list)
-      processed_exception_info_list.extends(self.check_code_number(xmom_path, exception_info_list))
+      processed_exception_info_list.extend(self.check_code_number(xmom_path, exception_info_list))
 
     for processed_exception_info in processed_exception_info_list:
       if verbose or (not verbose and processed_exception_info.status != 'ok'): 
