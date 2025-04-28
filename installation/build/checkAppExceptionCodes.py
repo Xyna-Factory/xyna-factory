34cae306b2207f98a938089437d124af69b7482f
Oliver Sünder <oliver.suender@gip.com>
2025-04-28 19:04:06 +0200

    1627-add-checkappexceptioncodespy

--- installation/build/checkAppExceptionCodes.py
+++ installation/build/checkAppExceptionCodes.py
@@ -84,7 +84,7 @@ class ExceptionXmlUtils:
       if exception_info.is_abstract == False and not exception_info.code:
         processed_exception_info = ProcessedExceptionInfo(exception_info.path, ProcessedExceptionInfoStatusConstants.NOK.value, 'Not defined')
       elif exception_info.code in all_exception_info_by_code and len(all_exception_info_by_code[exception_info.code]) > 1:
-        processed_exception_info = ProcessedExceptionInfo(exception_info.path, ProcessedExceptionInfoStatusConstants.NOK.value, 'Not unique' + exception_info.code)
+        processed_exception_info = ProcessedExceptionInfo(exception_info.path, ProcessedExceptionInfoStatusConstants.NOK.value, 'Not unique ' + exception_info.code)
       else:
         processed_exception_info = ProcessedExceptionInfo(exception_info.path, ProcessedExceptionInfoStatusConstants.OK.value, '')
       if processed_exception_info:
