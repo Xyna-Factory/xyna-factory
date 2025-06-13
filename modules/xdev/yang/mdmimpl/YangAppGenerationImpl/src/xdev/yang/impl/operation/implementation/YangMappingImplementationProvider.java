97dcf3a06b9c9e938f879ab96db0972c7e79edf1
Ulrich Seul <ulrich.seul@gip.com>
2025-06-13 19:01:33 +0200

    list handling

--- modules/xdev/yang/mdmimpl/YangAppGenerationImpl/src/xdev/yang/impl/operation/implementation/YangMappingImplementationProvider.java
+++ modules/xdev/yang/mdmimpl/YangAppGenerationImpl/src/xdev/yang/impl/operation/implementation/YangMappingImplementationProvider.java
@@ -45,7 +45,6 @@ public class YangMappingImplementationProvider implements ImplementationProvider
   public String createImpl(Document meta, List<String> inputVarNames) {
     StringBuilder result = new StringBuilder();
     List<ListConfiguration> listConfigs = ListConfiguration.loadListConfigurations(meta);
-    ListCounter globalCounter = new ListCounter();
     _tools.createVariables(result, meta, inputVarNames);
     result.append("\n");
     List<OperationMapping> mappings = OperationMapping.loadMappings(meta);
@@ -55,7 +54,7 @@ public class YangMappingImplementationProvider implements ImplementationProvider
     
     for (int i = 0; i < mappings.size(); i++) {
       OperationMapping mapping = mappings.get(i);
-      handleMapping(mapping, result, listConfigs, globalCounter);
+      handleMapping(mapping, result, listConfigs);
     }
     result.append("  xmcp.yang.YangMappingCollection coll2 = new xmcp.yang.YangMappingCollection();").append("\n");
     result.append("  coll2.overwriteContent(pathList);").append("\n");
@@ -70,67 +69,66 @@ public class YangMappingImplementationProvider implements ImplementationProvider
   }
   
   
-  private void handleMapping(OperationMapping mapping, StringBuilder result, List<ListConfiguration> listConfigs,
-                             ListCounter globalCounter) {
+  private void handleMapping(OperationMapping mapping, StringBuilder result, List<ListConfiguration> listConfigs) {
     ListCounter localCounter = new ListCounter();
     result.append("\n");
     result.append("  //").append(mapping.getMappingYangPath()).append(" -> ").append(mapping.getValue()).append("\n");
     List<MappingPathElement> mappingList = mapping.createPathList();
     result.append("  path = new xmcp.yang.YangMappingPath();").append("\n");
-    
+    result.append("  {").append("\n");
     for (int k = 0; k < mappingList.size(); k++) {
       MappingPathElement elem = mappingList.get(k);
       ListConfiguration listConfig = _tools.isDynamicList(mappingList.subList(0, k + 1), listConfigs);
       boolean isDynList = (listConfig != null);
       ListInfo listInfo = null;
       if (isDynList) {
-        globalCounter.count++;
         localCounter.count++;
-        listInfo = this.writeListStart(result, listConfig, globalCounter);
-        result.append("  path.addToPath(new xmcp.yang.YangMappingPathElement.Builder().elementName(\"")
+        listInfo = this.writeListStart(result, listConfig, localCounter);
+        result.append("    path.addToPath(new xmcp.yang.YangMappingPathElement.Builder().elementName(\"")
               .append(_tools.cleanupTag(elem.getYangPath())).append("\")").append(".listIndex(")
               .append(listInfo.counterVarname).append(")").append(".instance());").append("\n");
       } else if (OpImplTools.listKeywords.contains(elem.getKeyword())) {
         Optional<Integer> index = _tools.getOptionalConstListIndex(mappingList, k);
         if (index.isPresent()) {
-          result.append("  path.addToPath(new xmcp.yang.YangMappingPathElement.Builder().elementName(\"")
+          result.append("    path.addToPath(new xmcp.yang.YangMappingPathElement.Builder().elementName(\"")
                 .append(_tools.cleanupTag(elem.getYangPath())).append("\")").append(".listIndex(")
                 .append(index.get()).append(")").append(".instance());").append("\n");
         }
       } else if (!OpImplTools.hiddenYangKeywords.contains(elem.getKeyword())) {
-        result.append("  path.addToPath(new xmcp.yang.YangMappingPathElement.Builder().elementName(\"")
+        result.append("    path.addToPath(new xmcp.yang.YangMappingPathElement.Builder().elementName(\"")
             .append(_tools.cleanupTag(elem.getYangPath())).append("\")").append(".namespace(\"")
             .append(elem.getNamespace()).append("\")").append(".instance());").append("\n");
       }
     }
     String val = _tools.determineMappingString(mapping.getValue());
-    result.append("  path.setValue(").append(val).append(");").append("\n");
-    result.append("  pathList.add(path);").append("\n");
+    result.append("    path.setValue(").append(val).append(");").append("\n");
+    result.append("    pathList.add(path);").append("\n");
     closeLoops(result, localCounter);
+    result.append("  }").append("\n");
   }
   
   
   private ListInfo writeListStart(StringBuilder result, ListConfiguration listConfig, ListCounter counter) {
     ListInfo ret = new ListInfo();
-    result.append("  xmcp.yang.YangMappingPath backupPath_").append(counter.count).append( " = path;").append("\n");
+    result.append("    xmcp.yang.YangMappingPath backupPath_").append(counter.count).append( " = path;").append("\n");
     DynamicListLengthConfig dynListConfig = (DynamicListLengthConfig) listConfig.getConfig();
     String loopVareName = dynListConfig.getVariable();
     String loopVarPath = _tools.determineMappingValueObject(dynListConfig.getPath());
     String counterVarName = String.format("i_%d", counter.count);
     ret.counterVarname = counterVarName;
-    result.append("  List<?> ").append(loopVareName).append("_list = (List<?>)").append(loopVarPath).append(";").append("\n");
-    result.append("  for (int ").append(counterVarName).append(" = 0; ").append(counterVarName).append(" < ")
+    result.append("    List<?> ").append(loopVareName).append("_list = (List<?>)").append(loopVarPath).append(";").append("\n");
+    result.append("    for (int ").append(counterVarName).append(" = 0; ").append(counterVarName).append(" < ")
           .append(loopVareName).append("_list.size(); ").append(counterVarName).append("++) {").append("\n");
-    result.append("  Object ").append(loopVareName).append(" = ").append(loopVareName).append("_list.get(")
+    result.append("    Object ").append(loopVareName).append(" = ").append(loopVareName).append("_list.get(")
           .append(counterVarName).append(");").append("\n");
-    result.append("  path = backupPath_").append(counter.count).append( ".clone();").append("\n");
+    result.append("    path = backupPath_").append(counter.count).append( ".clone();").append("\n");
     return ret;
   }
   
   
   private void closeLoops(StringBuilder result, ListCounter counter) {
     for (int i = 0; i < counter.count; i++) {
-      result.append("  }").append("\n");
+      result.append("    }").append("\n");
     }
   }
   
