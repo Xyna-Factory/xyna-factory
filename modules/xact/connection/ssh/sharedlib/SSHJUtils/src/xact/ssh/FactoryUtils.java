cbe3a5600d8579b06b6131d848012010d5269487
Ulrich Seul <ulrich.seul@gip.com>
2025-07-28 10:31:32 +0200

    moved cipher factories

--- modules/xact/connection/ssh/sharedlib/SSHJUtils/src/xact/ssh/FactoryUtils.java
+++ modules/xact/connection/ssh/sharedlib/SSHJUtils/src/xact/ssh/FactoryUtils.java
@@ -24,14 +24,54 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 import com.hierynomus.sshj.key.KeyAlgorithm;
+import com.hierynomus.sshj.transport.cipher.BlockCiphers;
+import com.hierynomus.sshj.transport.cipher.GcmCiphers;
+import com.hierynomus.sshj.transport.cipher.StreamCiphers;
 import com.hierynomus.sshj.transport.mac.Macs;
 
 import net.schmizz.sshj.common.Factory.Named;
+import net.schmizz.sshj.transport.cipher.Cipher;
 import net.schmizz.sshj.transport.mac.MAC;
 
 
 public final class FactoryUtils {
 
+  public static final Map<String, Supplier<Named<Cipher>>> CipherFactories =
+      Map.ofEntries(Map.entry("aes128-ctr", BlockCiphers::AES128CTR),
+                    Map.entry("aes192-ctr", BlockCiphers::AES192CTR),
+                    Map.entry("aes256-ctr", BlockCiphers::AES256CTR),
+                    Map.entry("aes128-cbc", BlockCiphers::AES128CBC),
+                    Map.entry("aes192-cbc", BlockCiphers::AES192CBC),
+                    Map.entry("aes256-cbc", BlockCiphers::AES256CBC),
+                    Map.entry("blowfish-cbc", BlockCiphers::BlowfishCTR),
+                    Map.entry("twofish128-ctr", BlockCiphers::Twofish128CTR),
+                    Map.entry("twofish192-ctr", BlockCiphers::Twofish192CTR),
+                    Map.entry("twofish256-ctr", BlockCiphers::Twofish256CTR),
+                    Map.entry("twofish128-cbc", BlockCiphers::Twofish128CBC),
+                    Map.entry("twofish192-cbc", BlockCiphers::Twofish192CBC),
+                    Map.entry("twofish256-cbc", BlockCiphers::Twofish256CBC),
+                    Map.entry("twofish-cbc", BlockCiphers::TwofishCBC),
+                    Map.entry("serpent128-ctr", BlockCiphers::Serpent128CTR),
+                    Map.entry("serpent192-ctr", BlockCiphers::Serpent192CTR),
+                    Map.entry("serpent256-ctr", BlockCiphers::Serpent256CTR),
+                    Map.entry("serpent128-cbc", BlockCiphers::Serpent128CBC),
+                    Map.entry("serpent192-cbc", BlockCiphers::Serpent192CBC),
+                    Map.entry("serpent256-cbc", BlockCiphers::Serpent256CBC),
+                    Map.entry("idea-ctr", BlockCiphers::IDEACTR),
+                    Map.entry("idea-cbc", BlockCiphers::IDEACBC),
+                    Map.entry("cast128-ctr", BlockCiphers::Cast128CTR),
+                    Map.entry("cast128-cbc", BlockCiphers::Cast128CBC),
+                    Map.entry("3des-ctr", BlockCiphers::TripleDESCTR),
+                    Map.entry("3des-cbc", BlockCiphers::TripleDESCBC),
+
+                    Map.entry("aes128-gcm@openssh.com", GcmCiphers::AES128GCM),
+                    Map.entry("aes256-gcm@openssh.com", GcmCiphers::AES256GCM),
+
+                    Map.entry("arcfour", StreamCiphers::Arcfour),
+                    Map.entry("arcfour128", StreamCiphers::Arcfour128),
+                    Map.entry("arcfour256", StreamCiphers::Arcfour256)
+                    );
+  
   public static final Map<String, Supplier<Named<KeyAlgorithm>>> KeyAlgFactories =
       Map.ofEntries(Map.entry("ssh-rsa", com.hierynomus.sshj.key.KeyAlgorithms::SSHRSA),
                     Map.entry("ssh-rsa-cert-v01@openssh.com", com.hierynomus.sshj.key.KeyAlgorithms::SSHRSACertV01),
