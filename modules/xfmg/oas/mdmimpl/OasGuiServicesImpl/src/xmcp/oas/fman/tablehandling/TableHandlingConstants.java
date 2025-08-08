21bd95a1a5dc0dae0fa64578ea321ba52465400f
Ulrich Seul <ulrich.seul@gip.com>
2025-08-08 19:53:13 +0200

    filter and sort

---
+++ modules/xfmg/oas/mdmimpl/OasGuiServicesImpl/src/xmcp/oas/fman/tablehandling/TableHandlingConstants.java
@@ -0,0 +1,37 @@
+/*
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2025 Xyna GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+
+package xmcp.oas.fman.tablehandling;
+
+
+public class TableHandlingConstants {
+
+  public static class Paths {
+    public static final String GENERATED_RTC = "generatedRtc";
+    public static final String IMPLEMENTATION_RTC = "implementationRtc";
+    public static final String API_DATATYPE = "apiDatatype";
+    public static final String IMPLEMENTATION_DATATYPE = "implementationDatatype";
+    public static final String STATUS = "status";
+  }
+  
+  public static class SortVariants {
+    public static final String ASC = "asc";
+    public static final String DSC = "dsc";
+  }
+  
+}
