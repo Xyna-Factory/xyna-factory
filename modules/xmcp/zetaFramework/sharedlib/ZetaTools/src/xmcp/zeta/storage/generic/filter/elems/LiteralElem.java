1a788098ef53579bae95b0b65fe82ccfb05194dc
UlrichSeul-GIP <ulrich.seul@gip.com>
2025-09-11 18:20:09 +0200

    1858 zeta tools filtering for non modelled storables (#1868)
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * devel state
    
    * version numbers
    
    * code review
    
    * code review
    
    * code review
    
    * code review
    
    * code review
    
    * code review
    
    * code review
    
    * code review
    
    * usage example
    
    * usage example

---
+++ modules/xmcp/zetaFramework/sharedlib/ZetaTools/src/xmcp/zeta/storage/generic/filter/elems/LiteralElem.java
@@ -0,0 +1,81 @@
+/*
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2025 Xyna GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+
+package xmcp.zeta.storage.generic.filter.elems;
+
+import xmcp.zeta.storage.generic.filter.shared.FilterInputConstants;
+import xmcp.zeta.storage.generic.filter.shared.LiteralTools;
+
+
+public class LiteralElem extends RelationalOperandElem {
+
+  private final String _content;
+  private final boolean _isNumerical;
+  
+  
+  public LiteralElem(String input) {
+    if (input == null) { throw new IllegalArgumentException("LiteralElem: Content is null."); }
+    this._content = input;
+    _isNumerical = FilterInputConstants.NUMERICAL_PATTERN.matcher(_content).matches();
+  }
+  
+  
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+  
+  
+  @Override
+  public boolean containsWildcards() {
+    return false;
+  }
+  
+  @Override
+  public boolean isNumerical() {
+    return _isNumerical;
+  }
+
+  @Override
+  public String getContentString() {
+    return _content;
+  }
+
+  @Override
+  public boolean indicateAddWildcardAddEnd() {
+    return true;
+  }
+
+
+  @Override
+  public boolean indicateAddWildcardAddStart() {
+    return true;
+  }
+  
+  @Override
+  public String getContentAdaptedForSqlEquals() {
+    return new LiteralTools().adaptLiteralStringForEquals(_content);
+  }
+
+
+  @Override
+  public String getContentAdaptedForSqlLike() {
+    return new LiteralTools().adaptLiteralStringForLike(_content);
+  }
+
+}
