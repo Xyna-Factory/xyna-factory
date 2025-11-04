12572822063e89ed346f21a4b0d1902396088502
Cornelius Lippert | GIP <cornelius.lippert@gip.com>
2025-11-04 08:38:45 +0100

    cleanup, copyright notices

--- modules/xmcp/gitIntegration/mdmimpl/WorkspaceObjectManagementImpl/src/com/gip/xyna/xfmg/xfctrl/appmgmt/ApplicationXmlCompatibilityLayer.java
+++ modules/xmcp/gitIntegration/mdmimpl/WorkspaceObjectManagementImpl/src/com/gip/xyna/xfmg/xfctrl/appmgmt/ApplicationXmlCompatibilityLayer.java
@@ -1,7 +1,23 @@
+/*
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2025 Xyna GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
 package com.gip.xyna.xfmg.xfctrl.appmgmt;
 
 import java.io.File;
-import java.io.StringWriter;
 import java.util.List;
 import java.util.Map;
 
@@ -53,7 +69,6 @@ public class ApplicationXmlCompatibilityLayer {
 	public void createXml(File outputFile) throws ParserConfigurationException, Ex_FileAccessException {
 		xml.xmlVersion = ApplicationXmlHandler.XMLVERSION;
 		Document doc = xml.buildXmlDocument();
-		StringWriter sw = new StringWriter();
 		XMLUtils.saveDom(outputFile, doc);
 	}
 
