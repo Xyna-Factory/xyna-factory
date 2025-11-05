1a205527ae94bb4fc630a7d76deb7e1e153bf821
Lukas Fey <107985579+LukasFey-GIP@users.noreply.github.com>
2025-11-05 12:10:48 +0100

    [GitIntegration] Add SharedLibrary WorkspaceContentItem (#2017)

--- modules/xmcp/gitIntegration/mdmimpl/WorkspaceObjectManagementImpl/src/xmcp/gitintegration/impl/processing/TriggerProcessor.java
+++ modules/xmcp/gitIntegration/mdmimpl/WorkspaceObjectManagementImpl/src/xmcp/gitintegration/impl/processing/TriggerProcessor.java
@@ -51,15 +51,19 @@ import xmcp.gitintegration.Trigger;
 import xmcp.gitintegration.Reference;
 import xmcp.gitintegration.ReferenceData;
 import xmcp.gitintegration.ReferenceManagement;
+import xmcp.gitintegration.RepositoryManagement;
 import xmcp.gitintegration.WorkspaceContentDifference;
 import xmcp.gitintegration.impl.ItemDifference;
 import xmcp.gitintegration.impl.ReferenceComparator;
+import xmcp.gitintegration.impl.ReferenceConverter;
 import xmcp.gitintegration.impl.ReferenceUpdater;
 import xmcp.gitintegration.impl.XynaContentDifferenceType;
+import xmcp.gitintegration.impl.references.InternalReference;
 import xmcp.gitintegration.impl.references.ReferenceObjectType;
 import xmcp.gitintegration.impl.xml.ReferenceXmlConverter;
 import xmcp.gitintegration.storage.ReferenceStorable;
 import xmcp.gitintegration.storage.ReferenceStorage;
+import xprc.xpce.Workspace;
 
 
 
@@ -323,6 +327,11 @@ public class TriggerProcessor implements WorkspaceContentProcessor<Trigger> {
 
 
   private void createTrigger(Trigger item, long revision) {
+    ReferenceSupport refSupport = new ReferenceSupport();
+    ReferenceConverter refConverter = new ReferenceConverter();
+    Workspace workspaceName = new Workspace(ReferenceUpdater.getWorkspaceName(revision));
+    String pathToRepo = RepositoryManagement.getRepositoryConnection(workspaceName).getPath();
+
     StringSerializableList<String> ssl;
     ssl = StringSerializableList.autoSeparator(String.class, ":|/;\\@-_.+#=[]?§$%&!", ':');
     String[] jarFiles = ssl.deserializeFromString(item.getJarfiles()).toArray(new String[]{});
@@ -334,8 +343,15 @@ public class TriggerProcessor implements WorkspaceContentProcessor<Trigger> {
     File[] jarFilesArray = new File[jarFiles.length];
     int idx = 0;
     List<Reference> references = new ArrayList<>(item.getReferences());
+    List<InternalReference> internalReferences = new ArrayList<>();
+    for(Reference reference : references) {
+      InternalReference internalRef = refConverter.convert(reference);
+      internalRef.setPathToRepo(pathToRepo);
+      internalReferences.add(internalRef);
+    }
+    List<File> candidateFiles = refSupport.executeReferences(internalReferences);
     for (String jarFile : jarFiles) {
-      base.File file = ReferenceManagement.findReferencedJar(references, new File(jarFile).getName(), revision);
+      base.File file = candidateFiles.stream().filter(x -> x.getName().equals(jarFile)).map(x -> new base.File(x.getAbsolutePath())).findFirst().get();
       jarFilesArray[idx++] = new File(file.getPath());
     }
     try {
