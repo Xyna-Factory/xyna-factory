49e4440155e18b84fd5334e13cd536098dddf0a7
PatrickVonMassow-GIP <111737360+PatrickVonMassow-GIP@users.noreply.github.com>
2026-04-23 16:23:46 +0200

    Update patch version to 10.7.0.1 (#2292)

--- server/src/com/gip/xyna/update/Updater.java
+++ server/src/com/gip/xyna/update/Updater.java
@@ -1840,6 +1840,11 @@ public class Updater implements UpdaterInterface {
       ud.addFollowingBranchVersionsAsAllowedForUpdate(3); //10.6.x.x darf hier drauf updaten
       updates.add(ud);
 
+      // 10.7.0.1
+      Version v479 = new Version(v478).increaseToMajorVersion(4, 1);
+      ud = new UpdateJustVersion(v478, v479);
+      updates.add(ud);
+
       //ACHTUNG: bei updates in einem branch muss gewährleistet werden, dass alle späteren versionen (trunk, spätere branches)
       //         auf dem branch updaten können. bei updates, die in späteren versionen dann sonderbehandlungen im update-
       //         prozess benötigen, müssen die versionen, auf denen die updates passieren sollen, oben genau angegeben werden.
