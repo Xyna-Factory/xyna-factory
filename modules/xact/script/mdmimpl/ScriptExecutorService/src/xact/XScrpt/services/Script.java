b0cedc6a16d0d92ee96d2dfee939cc8b685d9e71
Ulrich Seul <ulrich.seul@gip.com>
2026-08-24 11:19:56 +0200

    Removed import

--- modules/xact/script/mdmimpl/ScriptExecutorService/src/xact/XScrpt/services/Script.java
+++ modules/xact/script/mdmimpl/ScriptExecutorService/src/xact/XScrpt/services/Script.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 import java.util.Timer;
 import java.util.TimerTask;
 
